b269efe9a0dd679600669b00ba258f1695e4df39
Thiago Puntar <thiagopuntar@mbp-de-thiago.lan>
2023-09-03 13:21:51 -0300

    add telescope and lsp basic config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,2 +1,34 @@
-require('telescope').load_extension('fzf')
+local telescope = require('telescope')
+local telescopeConfig = require("telescope.config")
 
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup({
+	defaults = {
+		-- `hidden = true` is not supported in text grep commands.
+		vimgrep_arguments = vimgrep_arguments,
+	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+	},
+})
+
+-- Extensions
+telescope.load_extension('fzf')
+telescope.load_extension('live_grep_args')
+
+-- Mappings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>st', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
--- lua/puntart/lazy.lua
+++ lua/puntart/lazy.lua
@@ -1,27 +1,63 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+	{
+		"navarasu/onedark.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			vim.cmd([[colorscheme onedark]])
+		end,
+	},
 	"tpope/vim-surround",
+	"github/copilot.vim",
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		dependencies = { 'nvim-lua/plenary.nvim' }
+		dependencies = { 
+			'nvim-lua/plenary.nvim',
+			{ 
+				"nvim-telescope/telescope-live-grep-args.nvim" ,
+				-- This will not install any breaking changes.
+				-- For major updates, this must be adjusted manually.
+				version = "^1.0.0",
+			},
+			{ 
+				'nvim-telescope/telescope-fzf-native.nvim',
+				build = 'make' 
+			}
+		}
 	},
-	{ 
-		'nvim-telescope/telescope-fzf-native.nvim',
-		build = 'make' 
-	}, 
-	"christoomey/vim-tmux-navigator"
+	"christoomey/vim-tmux-navigator",
+	{
+		"nvim-treesitter/nvim-treesitter",
+	},
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		dependencies = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	},
 
 }
 )
