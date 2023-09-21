fe9cb0b6d14858def59221111582e648b757dd65
Thiago Puntar <thiagopuntar@gmail.com>
2023-09-21 06:07:24 -0300

    feat: add copy and paste to system clipboard keymap

--- lua/puntart/remap.lua
+++ lua/puntart/remap.lua
@@ -2,6 +2,11 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex) -- open netrw / file explorer
 
+-- Remove search highlight
 vim.keymap.set("n", "<leader>h", ":noh<CR>")
+
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- Copy and paste to system clipboard
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("n", "<leader>y", '"+y')
