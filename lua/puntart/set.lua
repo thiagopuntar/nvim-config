799aad8bfe13d2a840198d6e61867e8c550a1bc6
Thiago Puntar <thiagopuntar@gmail.com>
2023-09-30 15:14:33 -0300

    add support to jenkinsfile

--- lua/puntart/set.lua
+++ lua/puntart/set.lua
@@ -4,3 +4,13 @@ vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+
+vim.api.nvim_exec(
+	[[
+  augroup JenkinsfileFileType
+    autocmd!
+    autocmd BufNewFile,BufRead Jenkinsfile set filetype=groovy
+  augroup END
+]],
+	true
+)
