7ce11515c52156d9e3f06ed47be73804d22daf4f
Thiago Puntar <thiagopuntar@gmail.com>
2023-09-30 15:13:06 -0300

    add terraform to lsp

--- lua/puntart/plugins/lspconfig.lua
+++ lua/puntart/plugins/lspconfig.lua
@@ -139,5 +139,10 @@ return {
 			capabilities = capabilities,
 			on_attach = on_attach,
 		})
+
+		lspconfig["terraformls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 	end,
 }
