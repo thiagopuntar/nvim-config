e9102db471240f36144eb1cefc47f6cae77efcfe
Thiago Puntar <thiagopuntar@gmail.com>
2023-10-28 10:06:43 -0300

    feat: add treesitter-context & lualine. Set cursorline

--- lua/puntart/set.lua
+++ lua/puntart/set.lua
@@ -4,6 +4,8 @@ vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.cursorline = true
 
 -- Ignore case when searching with /
 vim.o.ignorecase = true
