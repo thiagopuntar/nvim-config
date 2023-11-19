e3c5013acd70df43d77907a9dfedcc6217275c7b
Thiago Puntar <thiagopuntar@gmail.com>
2023-11-19 11:37:11 -0300

    feat: add lualine config

--- lua/puntart/plugins/lualine.lua
+++ lua/puntart/plugins/lualine.lua
@@ -5,6 +5,17 @@ return {
 	},
 	config = function()
 		local lualine = require("lualine")
-		lualine.setup()
+		lualine.setup({
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+					},
+				},
+
+				lualine_x = { "filetype" },
+			},
+		})
 	end,
 }
