8527a924b4d09bd36d70f77f3242a02d5fdc6a34
Thiago Puntar <thiagopuntar@gmail.com>
2023-11-19 11:22:40 -0300

    feat: nice remaps by Primeagen

--- lua/puntart/remap.lua
+++ lua/puntart/remap.lua
@@ -12,3 +12,16 @@ vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- move block as Vs code alt arrow down / arrow up
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Let cursor in the beginning of the statement after a J in normal mode
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Centralize view after navigation
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
